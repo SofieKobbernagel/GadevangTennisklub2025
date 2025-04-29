20efe99f80d5013840668f644d9411d7d1932c85
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-04-29 17:41:59 +0200

    nu kan man se de individuelle medlemmer hvis man er admin

--- GadevangTennisklub2025/Pages/Member/GetMember.cshtml.cs
+++ GadevangTennisklub2025/Pages/Member/GetMember.cshtml.cs
@@ -1,3 +1,4 @@
+using GadevangTennisklub2025.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,29 @@ namespace GadevangTennisklub2025.Pages.Member
 {
     public class GetMemberModel : PageModel
     {
-        public void OnGet()
+        private readonly IMemberService _memberService;
+     
+
+        public GetMemberModel(IMemberService memberService)
         {
+            _memberService = memberService;
         }
+
+        [BindProperty]
+        public GadevangTennisklub2025.Models.Member Member { get; set; }
+
+
+        public async Task<IActionResult> OnGetAsync(int member_Id)
+        {
+            Member = await _memberService.GetMemberById(member_Id);
+
+            if (Member == null)
+            {
+                return RedirectToPage("/Error");
+            }
+
+            return Page();
+        }
+
     }
 }
