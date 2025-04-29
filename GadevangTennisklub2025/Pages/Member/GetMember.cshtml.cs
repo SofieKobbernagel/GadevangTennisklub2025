6dceb3d80e385cb34e2ae55c2e7f18272db64ccf
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-04-29 17:16:09 +0200

    getallmembers

---
+++ GadevangTennisklub2025/Pages/Member/GetMember.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GadevangTennisklub2025.Pages.Member
+{
+    public class GetMemberModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
