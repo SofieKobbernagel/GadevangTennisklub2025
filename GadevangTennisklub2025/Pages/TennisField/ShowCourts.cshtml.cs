2bfd9dd79c6ba1819ba6e9d97edcf8dfe7c0d8ff
Kristoffer Aron Hendriksson <krh004@edu.zealand.dk>
2025-05-01 14:22:40 +0200

    CreateCourt dropdown and multiple classes

--- GadevangTennisklub2025/Pages/TennisField/ShowCourts.cshtml.cs
+++ GadevangTennisklub2025/Pages/TennisField/ShowCourts.cshtml.cs
@@ -1,5 +1,6 @@
 using GadevangTennisklub2025.Interfaces;
 using GadevangTennisklub2025.Models;
+using GadevangTennisklub2025.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -12,15 +13,27 @@ namespace GadevangTennisklub2025.Pages.TennisField
 
         public List<Models.TennisField> Courts { get; set; }
 
+        public List<SelectListItem> SelectList { get; set; }
+
         public ShowCourtsModel(ICourtService courtService)
         {
             _courtService = courtService;
+            SelectList = new List<SelectListItem>();
+        }
+
+        async Task LoadList()
+        {
+            foreach (var item in await _courtService.GetAllCourtsAsync())
+            {
+                SelectList.Add(new SelectListItem(item.Type, Convert.ToString(item.CourtId)));
+            }
         }
 
         public async Task<IActionResult> OnGetAsync()
         {
             try
             {
+                await LoadList();
                 Courts = await _courtService.GetAllCourtsAsync();
                 if (Courts == null)
                     return RedirectToPage("Index");
---
+++ GadevangTennisklub2025/Services/CourtTypeService.cs
@@ -0,0 +1,49 @@
+﻿using GadevangTennisklub2025.Interfaces;
+using GadevangTennisklub2025.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace GadevangTennisklub2025.Services
+{
+    public class CourtTypeService : ICourtTypeService
+    {
+        private string connectionString = Secret.ConnectionString;
+        private string selectAllCourtTypesSql = "select * From CourtType";
+
+
+        public async Task<List<CourtTypes>> GetAllCourtsAsync()
+        {
+            List<CourtTypes> courtTypes = new List<CourtTypes>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand(selectAllCourtTypesSql, connection);
+                    await command.Connection.OpenAsync();
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
+                    while (await reader.ReadAsync())
+                    {
+                        string type = reader.GetString("Type");
+
+                        CourtTypes c = new CourtTypes(type);
+
+                        courtTypes.Add(c);
+
+                    }
+                    reader.Close();
+                }
+                catch (SqlException sqlExp)
+                {
+                    Console.WriteLine("SQL ERROR: " + sqlExp.Message);
+                    Console.WriteLine("Stack Trace: " + sqlExp.StackTrace);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("GENERAL ERROR: " + ex.Message);
+                    Console.WriteLine("Stack Trace: " + ex.StackTrace);
+                }
+            }
+            return courtTypes;
+        }
+    }
+}
