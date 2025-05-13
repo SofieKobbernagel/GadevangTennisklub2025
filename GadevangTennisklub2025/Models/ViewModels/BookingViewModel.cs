c6101faeb3dec28e011fb6a6440e5e50127b361f
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-05-13 11:05:19 +0200

    se dine bookinger

---
+++ GadevangTennisklub2025/Models/ViewModels/BookingViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GadevangTennisklub2025.Models.ViewModels
+{
+    public class BookingViewModel
+    {
+        public Booking Booking { get; set; }
+        public string CourtName { get; set; }
+        public string PartnerName { get; set; }
+    }
+
+}
