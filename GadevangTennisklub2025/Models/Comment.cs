e9a06438a5daeab7a34e9cbdc24c2f6fc35a6d6f
smillingleeenfield <Magnushdguldager@gmail.com>
2025-05-19 10:21:12 +0200

    j

--- GadevangTennisklub2025/Models/Comment.cs
+++ GadevangTennisklub2025/Models/Comment.cs
@@ -1,10 +1,14 @@
-﻿namespace GadevangTennisklub2025.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GadevangTennisklub2025.Models
 {
     public class Comment
     {
         public int Id { get; set; } 
         public int BlogId { get; set; } 
-        public int MemberId { get; set; } 
+        public int MemberId { get; set; }
+        [Display(Name = "Kommentar")]
+        [Required(ErrorMessage = "Du skal skrive noget i din kommentar")]
         public string CommentContent { get; set; }
 
         public Comment() { }
